7c55b090a3de93b85a16775763aeedf2d97aeff5
Shira <shira@gmail.com>
2025-06-05 22:51:25 +0300

    ,,

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,93 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
+import org.example.MazePanel;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+public class Main {
+    public static final int DEFAULT_SIZE = 30;
+    public static final int WIDTH = 70;
+    public static final int HEIGHT = 70;
+    public static final int SQUARE_SIZE = 10;
+
+
+
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("start screen");
+        BufferedImage make = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+        window.setSize(1000, 1000);
+        window.setLocationRelativeTo(null);
+        window.setLayout(null);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+
+        try {
+            int w = safeValue(WIDTH);
+            int h = safeValue(HEIGHT);
+
+            HttpResponse<String> response = Unirest
+                    .get("https://app.seker.live/fm1/get-points")
+                    .queryString("width", w)
+                    .queryString("height", h)
+                    .asString();
+
+
+            JSONArray array = new JSONArray(response.getBody());
+            JSONObject object;
+            ArrayList<String> whitCoords = new ArrayList<>();// יצירת רשימה חדשה כדי לשמור על כל הנקדודת הלבנות
+
+            for (int i = 0; i < array.length(); i++) {
+                object = array.getJSONObject(i);
+                int x = object.getInt("x");
+                int y = object.getInt("y");
+                whitCoords.add(x + "." + y);// הכנסת הנתונים לתוך הרשימה
+
+                System.out.println("x:  " + x + "   " + " y:" + y);
+
+            }
+
+            for (int x = 0; x < w; x++) {
+                for (int y = 0; y < h; y++) {
+                    String str = x + "." + y;
+                    if (whitCoords.contains(str)) {
+                        make.setRGB(x, y, Color.WHITE.getRGB());
+                    } else {
+                        make.setRGB(x, y, Color.BLACK.getRGB());
+                    } // אני רק בודקת איזה משבצת צריכה להיות לבנה ואיזה שחורה ומכניסה לתוך התמונה את הפקודה
+                }
+            }
+
+            // צריך להכניס את התמונה לפנל ולהוסיף אותו לחלון
+            MazePanel mazePanel = new MazePanel(make);
+            window.add(mazePanel);
+
+
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
         }
+
+        window.setVisible(true);
+
+    }
+
+    public static int safeValue(Integer val) {
+        if (val == null) {
+            return DEFAULT_SIZE;
+        }
+        if (val < 5 || val > 100) {
+            return DEFAULT_SIZE;
+        }
+        return val;
     }
 }
--- src/main/java/org/example/MazePanel.java
+++ src/main/java/org/example/MazePanel.java
@@ -1,4 +1,30 @@
 package org.example;
 
-public class MazePanel {
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import java.nio.Buffer;
+import java.util.ArrayList;
+
+public class MazePanel extends JPanel {
+    private final BufferedImage image;
+    private static final int SQUARE_SIZE = 10;
+
+    public MazePanel(BufferedImage image) {
+        this.image = image;
+        this.setBounds(0, 0, image.getWidth() * SQUARE_SIZE, image.getHeight() * SQUARE_SIZE);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //g.drawImage(image,0,0,getWidth(),getWidth(),this);
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                g.setColor(new Color(image.getRGB(x, y)));
+                g.fillRect(x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+            }
+        }
+    }
 }
